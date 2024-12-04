4d35255d65f6004e6534cf997e233624141d1ed6
timurgordon <timurgordon@gmail.com>
2024-12-04 20:31:34 +0300

    hero rpc processor minimal implementation

---
+++ crystallib/hero/processor/procedure.v
@@ -0,0 +1,15 @@
+module processor
+
+// ProcedureCall struct representing a procedure invocation
+pub struct ProcedureCall {
+pub:
+    method string    // Method name (derived from OpenAPI path)
+    params string    // Parameters for the procedure
+}
+
+// ProcedureResponse struct representing the result of a procedure call
+pub struct ProcedureResponse {
+pub:
+    result string    // Response data
+    error  string    // Internal error message (if any)
+}
